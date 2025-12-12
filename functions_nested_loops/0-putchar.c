37001f0dcc921622e4dd45a74252d11d6386fe3e
ramiledov <ramiledov@gmail.com>
2025-12-12 13:48:08 +0000

    a

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - prints "_putchar" followed by a new line
+ * main - prints _putchar followed by a new line
  *
- * Return: 0
+ * Return: Always 0
  */
 int main(void)
 {
@@ -16,6 +16,5 @@ int main(void)
     _putchar('a');
     _putchar('r');
     _putchar('\n');
-
-    return 0;
+    return (0);
 }
---
+++ functions_nested_loops/main.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+int main(void)
+{
+    _putchar('H'); /* This is ONLY to test _putchar works */
+    _putchar('\n');
+    return (0);
+}
