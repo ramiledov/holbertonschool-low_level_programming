4ade2415793b7edfa72d5f8beb47f0b21d9c4611
ramiledov <ramiledov@gmail.com>
2025-12-16 14:12:14 +0000

    d

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -7,16 +7,16 @@
  */
 void print_alphabet_x10(void)
 {
-	int i;
-	char c;
+    int i;
+    char c;
 
-	for (i = 0; i < 10; i++)
-	{
-        	c = 'a';
+    for (i = 0; i < 10; i++)
+    {
+        c = 'a';
         while (c <= 'z')
         {
-            	_putchar(c);
-            	c++;
+            _putchar(c);
+            c++;
         }
         _putchar('\n');
     }
