8dbffa08838e9d64f27588470b7b5a13c3df43df
ramiledov <ramiledov@gmail.com>
2025-12-16 15:06:14 +0000

    f

---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+FGRTJURJRJGRo
+c
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * reset_to_98 - Updates the value of an integer to 98
+ * @n: Pointer to the integer to be updated
+ */
+void reset_to_98(int *n);
+
+#endif /* MAIN_H */
