b715e5fd9916dd8648c64b199fe6f9159b13761c
ramiledov <ramiledov@gmail.com>
2025-12-16 14:32:11 +0000

    task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: The result of a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
