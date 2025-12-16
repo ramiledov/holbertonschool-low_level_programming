6b51e6d26f1463556c2c984b5f61336ffed0625c
ramiledov <ramiledov@gmail.com>
2025-12-16 12:52:21 +0000

    g

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char c;
+	c = 'a';
+
+	while(c <= 'z'){
+		_putchar(c);
+		c++;
+		}
+	_putchar('\n');
+}
