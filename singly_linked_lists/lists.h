7769b6a5431618519faad51bf8c97949bb200a17
ramiledov <ramiledov@gmail.com>
2026-02-12 13:57:09 +0000

    listsh

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,8 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
-
 #include <stddef.h>
-
 /**
  * struct list_s - singly linked list node
  * @str: string (malloc'ed string)
@@ -17,7 +15,6 @@ typedef struct list_s
     unsigned int len;
     struct list_s *next;
 } list_t;
-
 /**
  * print_list - prints all elements of a list_t list
  * @h: pointer to the first node
@@ -25,7 +22,6 @@ typedef struct list_s
  * Return: the number of nodes
  */
 size_t print_list(const list_t *h);
-
 /**
  * list_len - returns the number of elements in a linked list_t
  * @h: pointer to the first node
@@ -33,7 +29,6 @@ size_t print_list(const list_t *h);
  * Return: the number of nodes
  */
 size_t list_len(const list_t *h);
-
 /**
  * add_node - adds a new node at the beginning of a list_t list
  * @head: pointer to the first node pointer
@@ -42,7 +37,6 @@ size_t list_len(const list_t *h);
  * Return: address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str);
-
 /**
  * add_node_end - adds a new node at the end of a list_t list
  * @head: pointer to the first node pointer
@@ -51,7 +45,6 @@ list_t *add_node(list_t **head, const char *str);
  * Return: address of the new element, or NULL if it failed
  */
 list_t *add_node_end(list_t **head, const char *str);
-
 /**
  * free_list - frees a list_t list
  * @head: pointer to the first node
@@ -59,6 +52,5 @@ list_t *add_node_end(list_t **head, const char *str);
  * Return: void
  */
 void free_list(list_t *head);
-
 #endif /* LISTS_H */
 
