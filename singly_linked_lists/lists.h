a436da234218b57736d87fe8eb9209e8c3231489
ramiledov <ramiledov@gmail.com>
2026-02-12 14:00:55 +0000

    lists

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stddef.h>
 
 /**
@@ -7,57 +8,20 @@
  * @str: string (malloc'ed string)
  * @len: length of the string
  * @next: pointer to the next node
- *
- * Description: singly linked list node for Holberton/ALX projects
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
-/**
- * print_list - prints all elements of a list_t list
- * @h: pointer to the first node
- *
- * Return: the number of nodes
- */
-size_t print_list(const list_t *h);
-
-/**
- * list_len - returns the number of elements in a linked list_t
- * @h: pointer to the first node
- *
- * Return: the number of nodes
- */
-size_t list_len(const list_t *h);
-
-/**
- * add_node - adds a new node at the beginning of a list_t list
- * @head: pointer to the first node pointer
- * @str: string to be duplicated in the new node
- *
- * Return: address of the new element, or NULL if it failed
- */
-list_t *add_node(list_t **head, const char *str);
-
-/**
- * add_node_end - adds a new node at the end of a list_t list
- * @head: pointer to the first node pointer
- * @str: string to be duplicated in the new node
- *
- * Return: address of the new element, or NULL if it failed
- */
-list_t *add_node_end(list_t **head, const char *str);
-
-/**
- * free_list - frees a list_t list
- * @head: pointer to the first node
- *
- * Return: void
- */
-void free_list(list_t *head);
+/* Function prototypes for Tasks 0–4 */
+size_t print_list(const list_t *h);                  /* Task 0 */
+size_t list_len(const list_t *h);                    /* Task 1 */
+list_t *add_node(list_t **head, const char *str);   /* Task 2 */
+list_t *add_node_end(list_t **head, const char *str); /* Task 3 */
+void free_list(list_t *head);                        /* Task 4 */
 
 #endif /* LISTS_H */
 
