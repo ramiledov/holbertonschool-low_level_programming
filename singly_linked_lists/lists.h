6417c7213b4411509c59b08fb4a80112f834ed3c
ramiledov <ramiledov@gmail.com>
2026-02-12 13:49:36 +0000

    listh

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -8,19 +8,57 @@
  * @str: string (malloc'ed string)
  * @len: length of the string
  * @next: pointer to the next node
+ *
+ * Description: singly linked list node for Holberton/ALX projects
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
-/* Function prototypes for Tasks 0–4 */
-size_t print_list(const list_t *h);                  /* Task 0 */
-size_t list_len(const list_t *h);                    /* Task 1 */
-list_t *add_node(list_t **head, const char *str);   /* Task 2 */
-list_t *add_node_end(list_t **head, const char *str); /* Task 3 */
-void free_list(list_t *head);                        /* Task 4 */
+/**
+ * print_list - prints all elements of a list_t list
+ * @h: pointer to the first node
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h);
+
+/**
+ * list_len - returns the number of elements in a linked list_t
+ * @h: pointer to the first node
+ *
+ * Return: the number of nodes
+ */
+size_t list_len(const list_t *h);
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: pointer to the first node pointer
+ * @str: string to be duplicated in the new node
+ *
+ * Return: address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str);
+
+/**
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: pointer to the first node pointer
+ * @str: string to be duplicated in the new node
+ *
+ * Return: address of the new element, or NULL if it failed
+ */
+list_t *add_node_end(list_t **head, const char *str);
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the first node
+ *
+ * Return: void
+ */
+void free_list(list_t *head);
 
 #endif /* LISTS_H */
+
